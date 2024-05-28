d99afa2f2b7f0b158d9cf5caedcd0ff489670fa4
Diman Todorov <diman.todorov@outlook.com>
2024-05-27 22:14:48 -0700

    writing test image

--- main.c
+++ main.c
@@ -28,11 +28,22 @@ int main(int argc, char **argv)
 
     double image[width * height * 3];
 
-    for (int pixel = 0; pixel < width * height; pixel++)
+    for (int row = 0; row < height; row++)
     {
-        for (int color = 0; color < 3; color++)
+        for (int col = 0; col < width; col++)
         {
-            image[pixel * 3 + color] = (double)pixel / (width * height);
+            int pixel = row * width + col;
+            double r = (double)row / (width - 1);
+            double g = (double)col / (height - 1);
+            double b = 0.0;
+
+            int ir = (int)255.999 * r;
+            int ig = (int)255.999 * g;
+            int ib = (int)255.999 * b;
+
+            image[pixel * 3 + 0] = ir;
+            image[pixel * 3 + 1] = ig;
+            image[pixel * 3 + 2] = ib;
         }
     }
 
