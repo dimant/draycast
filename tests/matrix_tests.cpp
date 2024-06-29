44a0f1ac426f58270ffd6399739002dfcdc4cf40
Diman Todorov <diman.todorov@outlook.com>
2024-06-29 14:37:46 -0700

    more matrices

--- tests/matrix_tests.cpp
+++ tests/matrix_tests.cpp
@@ -2,13 +2,45 @@
 
 #include "../src/matrix.h"
 
-TEST(matrix_access_test)
+TEST(matrix4_access_test)
 {
     Matrix<4> m;
 
     for (size_t row = 0; row < 4; row++)
     {
         for (size_t col = 0; col < 4; col++)
+        {
+            m.set(row, col, 1.0f);
+            ASSERT_FLOAT_EQ(m.get(row, col), 1.0f);
+        }
+    }
+
+    return true;
+}
+
+TEST(matrix3_access_test)
+{
+    Matrix<3> m;
+
+    for (size_t row = 0; row < 3; row++)
+    {
+        for (size_t col = 0; col < 3; col++)
+        {
+            m.set(row, col, 1.0f);
+            ASSERT_FLOAT_EQ(m.get(row, col), 1.0f);
+        }
+    }
+
+    return true;
+}
+
+TEST(matrix2_access_test)
+{
+    Matrix<2> m;
+
+    for (size_t row = 0; row < 2; row++)
+    {
+        for (size_t col = 0; col < 2; col++)
         {
             m.set(row, col, 1.0f);
             ASSERT_FLOAT_EQ(m.get(row, col), 1.0f);
